62220d62c6ed52edcd5f434f8b52cd16c061486f
Thomas Centa <centathomas@gmail.com>
2017-11-21 11:41:40 -0800

    Revert "Moved stuff out of default package"
    This reverts commit 7cd430150e9c740e215a51768a624e2928461258.

--- Hackerrank/src/Functions/ClockFunctions.java
+++
@@ -1,9 +0,0 @@
-package Functions;
-
-public class ClockFunctions {
-	public static int clockSubtract(int subtractFrom, int subtractThis, int mod) {
-		long i2 = subtractFrom;
-		long j2 = subtractThis;
-		return (int)((((i2-j2) % mod)+mod) % mod);
-	}
-}
