50c3e38c35ec10b08bc9090e93eda0506c7706d8
Thomas Centa <centathomas@gmail.com>
2017-11-21 11:05:49 -0800

    nada

--- Hackerrank/Tests/modfunctiontests/modAddTests.java
+++ Hackerrank/Tests/modfunctiontests/modAddTests.java
@@ -4,10 +4,25 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import Functions.ModFunctions;
+
 public class modAddTests {
 
 	@Test
 	public void testBothMax(){
-		
+		int addOne = Integer.MAX_VALUE;
+		int addTwo = Integer.MAX_VALUE;
+		int mod = Integer.MAX_VALUE;
+		int expected = 0;
+		assertEquals(expected, ModFunctions.modAdd(addOne, addTwo, mod));
+	}
+	
+	@Test
+	public void testBothMin(){
+		int addOne = Integer.MIN_VALUE;
+		int addTwo = Integer.MIN_VALUE;
+		int mod = Integer.MAX_VALUE;
+		int expected = -2;
+		assertEquals(expected, ModFunctions.modAdd(addOne, addTwo, mod));
 	}
 }
